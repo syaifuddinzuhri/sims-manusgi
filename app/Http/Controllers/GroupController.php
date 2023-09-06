558cfacdca88059c560128e37ee676dd60749709
syaifuddinzuhri <mochammadsyaifuddinz@gmail.com>
2023-09-06 23:56:48 +0700

    edit datatable

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -39,9 +39,9 @@ class GroupController extends Controller
                 ->addIndexColumn()
                 ->addColumn('action', function ($data) {
                     $button = '<div class="btn-group" role="group">';
-                    $button .= '<a href="' . route('grup.edit', encryptData($data->id)) . '" class="btn btn-sm btn-info">
+                    $button .= '<a href="' . route('grup.edit', encryptData($data->id)) . '" class="btn btn-sm btn-info" data-toggle="tooltip" data-placement="bottom" title="Edit">
                         <i class="fa fa-edit" aria-hidden="true"></i> </a>';
-                    $button .= '<button type="button" data-toggle="modal" data-target="#modal-delete" data-backdrop="static" data-keyboard="false" class="btn btn-sm btn-danger delete"><i class="fa fa-trash" aria-hidden="true"></i></button>';
+                    $button .= '<button type="button" data-toggle="modal" data-target="#modal-delete" data-backdrop="static" data-keyboard="false" class="btn btn-sm btn-danger delete" data-toggle="tooltip" data-placement="bottom" title="Hapus"><i class="fa fa-trash-alt" aria-hidden="true"></i></button>';
                     $button .= '</div>';
                     return $button;
                 })
