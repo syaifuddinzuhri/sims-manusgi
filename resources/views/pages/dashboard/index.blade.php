309cd090572e0bf2521be723135029d357aed16e
Mochammad Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2023-09-12 16:11:00 +0700

    update dashboard

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -8,15 +8,6 @@
             <div class="section-header">
                 <h1>Selamat Datang, <span class="text-primary">{{ authUser()->name }}</span></h1>
             </div>
-            <div class="row">
-                <div class="col-12">
-                    <form action="{{ route('dashboard.submit') }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <input type="file" name="file">
-                        <button type="submit">submit</button>
-                    </form>
-                </div>
-            </div>
         </section>
     </div>
 @endsection
